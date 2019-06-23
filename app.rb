cd0db78915e5a064ba31f6f4ab7fbdae78228b5a
Jim Hough <teachlife@outlook.com>
2019-06-23 23:50:03 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,6 +3,10 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   # This is a sample static route.
+  get '/' do
+    "Hello!"
+  end
+
   get '/hello' do
     "Hello World!"
   end
